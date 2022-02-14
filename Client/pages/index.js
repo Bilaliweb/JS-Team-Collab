95a4bc5a09abb7fe4ade5998e54ed48bacbbd6c9
Bilaliweb <muhammadbilalshahid952@gmail.com>
2022-02-14 13:15:34 +0500

    Target file updated

--- Client/pages/index.js
+++ Client/pages/index.js
@@ -1,8 +1,10 @@
 import { BasicTables } from '/src/components/BasicTable'
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 let pointer = 0;
-
 let tables = ['task', 'members', 'labels', 'invoices', 'attachments']
+let finder = '';
+let finderArr = [];
+let index = 0;
 
 //For initial Fetching 
 export const getServerSideProps = async () => {
@@ -17,7 +19,6 @@ export const getServerSideProps = async () => {
 }
 
 //For Fetching 
-
 export const getData = async (pointer) => {
 
   let res = await fetch(`http://localhost:8080/${tables[pointer]}`);
@@ -43,20 +44,60 @@ export default function Home({ data }) {
 
     <div>
 
+      <h1 className='text-center text-bermuda text-5xl font-bold font-body my-8'>React Table
+        <h1 className='text-center text-gray-900 text-2xl my-4 font-light font-body'>Fetching Data and Displaying</h1></h1>
 
-      <h1 className='text-center text-bermuda text-5xl font-bold mt-8'>React-Table
-        <h1 className='text-center text-gray-900 text-2xl mt-4 font-light m-8'>Fetching Data and Displaying</h1></h1>
+      <h2 className='text-center text-bermuda text-6xl font-bold font-body'>{title.toUpperCase()}</h2>
 
-      <h2 className='text-center  text-bermuda text-4xl font-bold'>Table of {title}</h2>
+      <div className='w-72 mx-auto flex flex-col'>
+        <h1 className='text-3xl font-bold text-gray-400 text-center p-4'>Search by ID</h1>
+        <input id="input" type="text" className='px-1 shadow-xl border border-gray-900' onChange={(change) => {
+          console.log(change.nativeEvent.data)
+          if (change.nativeEvent.data !== null) {
+            finderArr = []
+            finder += change.nativeEvent.data
+            console.log(finder)
+            getData(pointer).then(v => {
+              v.props.Btndata.forEach((e) => {
 
+                if (index == 0)
+                  if (finder[index] === String(e.id)[index]) {
+                    finderArr.push(e)
+                  }
 
+                if (index == 1) {
+                  if (finder[index] === String(e.id)[index]) {
+                    if (finder[index - 1] === String(e.id)[index - 1])
+                      finderArr.push(e)
+
+                  }
+                }
+
+              })
+            }).then(() => {
+              console.log(finderArr)
+              setFetched(finderArr)
+              index++
+            })
+          }
+          else {
+            let inputField = document.querySelector('#input')
+            inputField.value = ''
+            finder = ''
+            finderArr = []
+            getData(pointer).then((v) => { setFetched(v.props.Btndata) })
+            index = 0
+          }
+
+        }} />
+      </div>
 
       <div className='w-[80%] h-full bg-bermuda text-center mx-auto mt-4 rounded-xl shadow-xl'>
         <BasicTables sendData={[fetched, pointer]} />
       </div>
 
       <div className='flex flex-row-reverse w-96 mx-auto'>
-        <div className="
+        <div className="font-body
       m-8 p-4 text-bermuda w-40 text-bold border-2 border-bermuda bg-white mx-auto text-center shadow-lg hover:shadow-xl hover-p-8 rounded-xl text-xl hover:transition-all transition-all cursor-pointer" onClick={() => {
             if (pointer !== tables.length - 1)
               pointer++;
@@ -68,7 +109,7 @@ export default function Home({ data }) {
           NEXT PAGE..
         </div>
 
-        <div className="
+        <div className="font-body
       m-8 p-4 text-white w-40 text-bold bg-bermuda mx-auto text-center shadow-lg hover:shadow-xl hover-p-8 rounded-xl text-xl hover:transition-all transition-all cursor-pointer" onClick={() => {
             if (pointer !== 0)
               pointer--;
@@ -77,9 +118,7 @@ export default function Home({ data }) {
             getData(pointer).then(v => setFetched(v.props.Btndata))
             setTitle(tables[pointer])
           }}>
-
           PREV PAGE..
-
         </div>
       </div>
 
@@ -96,17 +135,13 @@ function Sorter(data) {
 
   data.forEach((e) => {
     for (let key in e) {
-
       if (key === 'id')
         sorter.push(e[key]);
-
     }
   })
-
   sorter.sort((a, b) => {
     return a - b
   });
-
   data.forEach(() => {
     data.forEach(e => {
       for (let key in e) {
@@ -119,7 +154,5 @@ function Sorter(data) {
       }
     })
   })
-
   return dataArr;
-
 }
